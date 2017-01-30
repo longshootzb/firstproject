e41444bf5279f8fa40bc8d8e8dc662d0fe2de752
longshootzb <longshootzb@users.noreply.github.com>
2017-01-30 15:08:25 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
-    });
-}
+var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
+});
+var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    anchor: new Microsoft.Maps.Point(12, 39) });
+map.entities.push(pushpin);
