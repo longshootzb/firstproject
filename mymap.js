c5a7847c8586b288f38c4c7fead5e8824a455019
longshootzb <longshootzb@users.noreply.github.com>
2017-01-30 13:13:48 +0800

    Delete mymap.js

--- mymap.js
+++
@@ -1,6 +0,0 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
-    });
-}
