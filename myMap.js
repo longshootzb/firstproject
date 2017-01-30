ae36ed8b5958b32e17d582a543931ee174ab8360
longshootzb <longshootzb@users.noreply.github.com>
2017-01-30 18:01:50 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,9 +1,9 @@
 function loadMapScenario() {
-                var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-                    credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
-                });
-                var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/longshootzb/pokeman_week3/raw/master/pokemon/',
-                    anchor: new Microsoft.Maps.Point(12, 39) });
-                map.entities.push(pushpin);
-                
-            }
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
+    });
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/longshootzb/firstproject/master/pokemon/1.png',
+        anchor: new Microsoft.Maps.Point(12, 39) });
+    map.entities.push(pushpin);
+
+}
