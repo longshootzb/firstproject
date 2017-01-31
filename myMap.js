0d7683e5949ab6b4632f701aa3e42f2fe605c48d
longshootzb <longshootzb@users.noreply.github.com>
2017-01-31 08:13:47 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -17,6 +17,13 @@ map_manager.map_items = [
       "latitude" : 43.8396951,
     }
 ]
+
+function loadMapScenario() {
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
+    });  
+}
+// 1. Define pokemon data format, create mock pokemon data
 function get_counter_down_time_from_expire_epoch(epoch) {
   var now_time = new Date().getTime() / 1000;
   var time_left = epoch / 1000 - now_time;   // unit: second
@@ -25,18 +32,28 @@ function get_counter_down_time_from_expire_epoch(epoch) {
   return minute + ":" + second;
 }
 
-function loadMapScenario() {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
-    });
-    map_manager.map=map;
+// 2. Create pokemon image on map
+function get_pokemon_layer_from_map_items(map_items) {
+    var layer = new Microsoft.Maps.Layer();
     var pushpins = []
     for (var i in map_manager.map_items) {
-    var map_item = map_manager.map_items[i];
-    var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
-                                             { icon: 'https://raw.githubusercontent.com/longshootzb/firstproject/master/pokemon/' + map_item["pokemon_id"] + '.png',
-                                              title: get_counter_down_time_from_expire_epoch(map_item["expire"]) });
-    map.entities.push(pushpin);
+        var map_item = map_manager.map_items[i];
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
+                                                 { icon: 'https://raw.githubusercontent.com/longshootzb/firstproject/master/pokemon/' + map_item["pokemon_id"] + '.png',
+                                                  title: get_counter_down_time_from_expire_epoch(map_item["expire"]) });
+        pushpins.push(pushpin)
     }
-
+    layer.add(pushpins);
+    return layer;
 }
+
+// 3. Add pokemon counter down refresh.
+function refresh_pokemon_layer() {
+  // Prepare new layer
+  var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
+  // Remove old layer
+  map_manager.map.layers.clear()
+  // Add new layer
+  map_manager.map.layers.insert(pokemon_layer);
+}
+window.setInterval(refresh_pokemon_data, 1000);
