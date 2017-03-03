58a27cf3e582fbb9f6c224b000eff06410a92e5e
longshootzb <longshootzb@users.noreply.github.com>
2017-03-04 06:50:34 +0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -38,7 +38,7 @@ function loadMapScenario() {
         credentials: 'AmGfXoPF5L4OcyP3y1T7R9POld5vCh6OkxuB9eRsNaqVvd6J6Z3hHZUWyh79THjC'
     }); 
     map_manager.map=map;
-    window.setInterval(query_pokemon_data, 5000);
+    window.setInterval(query_pokemon_data, 1000);
     window.setInterval(refresh_pokemon_layer, 1000);
 }
 // 1. Define pokemon data format, create mock pokemon data
